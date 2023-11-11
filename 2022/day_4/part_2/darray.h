77e0cc9b360cd320670531e0844030e802cb152d
maxmakovskiy <makovskiymax@gmail.com>
2023-11-11 02:22:43 +0100

    4th day second part in C - partitially solved

---
+++ 2022/day_4/part_2/darray.h
@@ -0,0 +1,46 @@
+#ifndef _D_ARRAY_H_
+#define _D_ARRAY_H_
+
+#include <stdlib.h>
+#include <stddef.h>
+
+typedef struct BufHdr {
+  size_t len;
+  size_t cap;
+  char buf[0];
+} BufHdr;
+
+
+#define MAX(x, y) ((x) <= (y) ? (y) : (x))
+
+#define byte char
+#define buf__hdr(b) ((BufHdr*)((byte*)b - offsetof(BufHdr, buf)))
+#define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
+#define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow((b), buf_len(b) + n, sizeof(*(b)))))
+
+#define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
+#define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
+#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
+#define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
+
+
+void* buf__grow(const void* buf, size_t new_len, size_t elem_size) {
+  // Why?
+  size_t new_cap = MAX(1 + 2*buf_cap(buf), new_len);
+  // our data + prefix(len and capacity)
+  size_t new_size = offsetof(BufHdr, buf) + new_cap*elem_size;
+
+  BufHdr* new_hdr;
+
+  if (buf) {
+    new_hdr = realloc(buf__hdr(buf), new_size);
+  } else { // for first push
+    new_hdr = malloc(new_size);
+    new_hdr->len = 0;
+  }
+  new_hdr->cap = new_cap;
+  return new_hdr->buf;
+}
+
+
+#endif
---
+++ 2022/day_4/part_2/main.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdint.h>
+#include "darray.h"
+
+
+// + new line feed?
+// So, new line feed it is +1
+#define DATA_STR_L (12)
+
+
+int clean_line(char* line, int len) {
+  int i;
+  int actual_len = 0;
+  for (i = 0; i < len; i++) {
+    if (line[i] == '\n') {
+      actual_len = i+1;
+      break;
+    }
+  }
+  for (; i < len; i++) {
+    line[i] = '\0';
+  }
+  return actual_len;
+}
+
+
+int pow(int base, int exp) {
+  int result = 1;
+  int i = exp;
+  for (; i > 0; i--) {
+    result *= base;
+  }
+  return result;
+}
+
+
+// "23" -> int[2, 3] -> 2 * 10^1 + 3 * 10^0
+int str_to_int(const char* input, int length) {
+  int k;
+  int result = 0;
+  for (k = 0; k < length; k++) {
+    result += (((int)input[k]) - 0x30) * pow(10, length-k-1);
+  }
+  return result;
+}
+
+int is_numeric(char ch) {
+  return (0x30 <= ch && ch <= 0x39) ? 1 : 0;
+}
+
+int process_line(const char* line, int length) {
+  // 8-41,8-79
+  // we would have this integer types
+  // 8 = 0x00'00'00'08  || 0x08
+  // 41 = 0x00'00'00'29 || 0x29
+  // 8 = 0x00'00'00'08  || 0x08
+  // 79 = 0x00'00'00'4F || 0x4F
+  // let's reduce it to byte
+  // then let's compose it into one 32-bit int number
+  // 0x08
+  //     0x29
+  //         0x08
+  //             0x4F
+  // 0x0829084F as result
+  // but we need 64 bit
+  
+  int i;
+
+  char buf[2];
+  int buf_iter = 0;
+
+  int res_iter = 0;
+  uint64_t result = 0;
+
+  for(i = 0; i < length; i++) {
+    if (is_numeric(line[i])) {
+      buf[buf_iter++] = line[i];
+    } else {
+      uint64_t number = (uint64_t)str_to_int(buf, buf_iter);
+      uint64_t t = (number << ((3 - res_iter) * sizeof(uint64_t)));
+      result |= t;
+      res_iter++;
+      buf_iter = 0;
+    }
+  }
+
+  return result;
+}
+
+
+void print_buffer(const char* p, int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    printf("%c", (p[i]));
+  }
+  printf("\n");
+}
+
+int main(int argc, char** argv) {
+  int f_descr = open("input.txt", O_RDONLY);
+  if (f_descr < 0) {
+    printf("Can't open file\n");
+    return -1;
+  }
+
+  char buffer[DATA_STR_L];
+
+    
+  int k;
+  int next_jump = 0;
+
+  while(k = read(f_descr, buffer, DATA_STR_L)) {
+    int clean_len = clean_line(buffer, DATA_STR_L);
+    next_jump += clean_len;
+    print_buffer(buffer, clean_len);
+    int r = process_line(buffer, clean_len);
+    printf("0x%08x\n", r);
+    lseek(f_descr, next_jump, SEEK_SET);
+
+  }
+
+  
+  close(f_descr);
+  
+  return 0;
+}
+
+/*
+int main(void) {
+  int* ptr = NULL;
+  enum { N = 2048 };
+  int i;
+  for (i = 0; i < N; i++) {
+    buf_push(ptr, i);
+  }
+  buf_free(ptr);
+ 
+  return 0;
+}
+*/
+
+/*
+#include <stddef.h>
+
+typedef struct example {
+  int field1;
+  char field2;
+  char trick[0];
+} example;
+
+int main(void) {
+  printf("size of struct example %ld\n", sizeof(example));
+  printf("field1 offset %ld\n", offsetof(example, field1));
+  printf("field2 offset %ld\n", offsetof(example, field2));
+  printf("tricky field3 has offset %ld\n", offsetof(example, trick));
+  printf("byte's aligning %ld\n",
+     sizeof(example)-sizeof(int)-sizeof(char)-sizeof(char));
+  return 0;
+}
+*/
