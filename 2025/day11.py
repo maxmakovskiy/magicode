b260264ab8f18c881632cefd177f16b4152866b5
maxmakovskiy <makovskiymax@gmail.com>
2025-12-20 11:21:09 +0100

    2025: day 11 refactoring

--- 2025/day11.py
+++ 2025/day11.py
@@ -1,56 +1,18 @@
-def explore(vertex, adjacency_list, visited, counters):
-    visited[vertex] = True
-
-    for v in adjacency_list[vertex]:
-        if v == "out":
-            return 1
-        elif not visited[v]:
-            counters[v] += explore(v, adjacency_list, visited, counters)
-
-    counters[vertex] = sum([counters[v] for v in adjacency_list[vertex]])
-
-    return counters[vertex]
-
-def explore_3(vertex, target, adjacency_list, visited, counters):
+def explore(vertex, target, adjacency_list, visited, counters):
     visited[vertex] = True
 
     for v in adjacency_list[vertex]:
         if v == target:
             return 1
         elif not visited[v]:
-            counters[v] += explore_3(v, target, adjacency_list, visited, counters)
+            counters[v] += explore(v, target, adjacency_list, visited, counters)
 
     counters[vertex] = sum([counters[v] for v in adjacency_list[vertex]])
 
     return counters[vertex]
 
 
-
-def explore_2(vertex, adjacency_list, visited, counters, dac, fft):
-    if dac and fft:
-        visited[vertex] = True
-
-    if vertex == "dac":
-        dac = True
-
-    if vertex == "fft":
-        fft = True
-
-    for v in adjacency_list[vertex]:
-        if v == "out" and dac and fft:
-            return 1
-        elif v == "out":
-            return 0
-        elif not visited[v]:
-            counters[v] += explore_2(v, adjacency_list, visited, counters, dac, fft)
-
-    counters[vertex] = sum([counters[v] for v in adjacency_list[vertex]])
-
-    return counters[vertex]
-
-
-
-def dfs(adjacency_list, label):
+def dfs(adjacency_list, *, label, target):
     visited: dict[str, bool] = dict()
     counters: dict[str, int] = dict()
 
@@ -60,43 +22,7 @@ def dfs(adjacency_list, label):
 
     s = 0
     for v in adjacency_list[label]:
-        s += explore(v, adjacency_list, visited, counters)
-
-    return s
-
-
-def dfs_3(adjacency_list, *, label, target):
-    visited: dict[str, bool] = dict()
-    counters: dict[str, int] = dict()
-
-    for vertex in adjacency_list.keys():
-        visited[vertex] = False
-        counters[vertex] = 0
-
-    s = 0
-    for v in adjacency_list[label]:
-        s += explore_3(v, target, adjacency_list, visited, counters)
-
-    return s
-
-
-
-def dfs_2(adjacency_list, label):
-    visited: dict[str, bool] = dict()
-    counters: dict[str, int] = dict()
-
-    dac_visited = False
-    fft_visited = False
-
-    for vertex in adjacency_list.keys():
-        visited[vertex] = False
-        counters[vertex] = 0
-
-    s = 0
-    for v in adjacency_list[label]:
-        s += explore_2(v, adjacency_list, visited, counters, dac_visited, fft_visited)
-        dac_visited = False
-        fft_visited = False
+        s += explore(v, target, adjacency_list, visited, counters)
 
     return s
 
@@ -124,7 +50,7 @@ def part_1():
 
         adjacency_list["out"] = []
 
-        return dfs(adjacency_list, "you")
+        return dfs(adjacency_list, label="you", target="out")
 
 
 def part_2():
@@ -157,16 +83,15 @@ def part_2():
     adjacency_list["out"] = []
 
     return (
-              dfs_3(adjacency_list, label="svr", target="fft")
-            * dfs_3(adjacency_list, label="fft", target="dac")
-            * dfs_3(adjacency_list, label="dac", target="out")
+              dfs(adjacency_list, label="svr", target="fft")
+            * dfs(adjacency_list, label="fft", target="dac")
+            * dfs(adjacency_list, label="dac", target="out")
     )
 
 
 def main():
     print(f"Part 1: {part_1()}")
     print(f"Part 2: {part_2()}")
-
 
 
 if __name__ == "__main__":
