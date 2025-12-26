9fa98a875e1dce7cfed047ac11d5eaf87f76bc87
maxmakovskiy <makovskiymax@gmail.com>
2025-12-26 15:54:58 +0100

    2023 : day 3 part 1 wip

--- 2023/day_3/bin/main.ml
+++ 2023/day_3/bin/main.ml
@@ -11,28 +11,25 @@ let map_rows : string list = [
   ".664.598.."
 ]
 
-type value =
-  | Integer of int
-  | Symbol of char
-
-type coord =
-  | Point of int
-  | Range of int * int
-
 type vertex = {
-  v : value;
-  x : coord;
-  y : coord;
+  v : int;
+  x : int list;
+  y : int;
+}
+
+type symbol = {
+  x: int;
+  y: int;
 }
 
 let is_digit c = c >= '0' && c <= '9'
 let is_anchor c = c <> '.'
 
-let rec scan i acc line ~y : vertex list =
+let rec scan i line ~values ~symbols ~y : vertex list * symbol list  =
   let len = String.length line in
     if i >= len then
       (* acc is built backwards since List.rev_append (::) is used *)
-      List.rev acc
+      List.rev values, List.rev symbols
 
     else if is_digit line.[i] then
       let j = ref i in 
@@ -43,33 +40,34 @@ let rec scan i acc line ~y : vertex list =
 
         (* Parse consumed digits to the int *)
         let value = int_of_string (String.sub line i (!j - i)) in
-          scan !j ({v=Integer value; x=Range (i, !j); y=Point y} :: acc) line ~y:y
+          scan !j line ~values:({v=value; x=(List.init (!j - i) (fun dx -> i + dx)); y=y} :: values) ~symbols:symbols ~y:y
 
     else if is_anchor line.[i] then
-      scan (i + 1) ({v=Symbol line.[i]; x=Point i; y=Point y} :: acc) line ~y:y
+      scan (i + 1) line ~values:values ~symbols:({x=i; y=y} :: symbols) ~y:y
     else
-      scan (i + 1) acc line ~y:y
-
-let process_row row_idx row : vertex list = scan ~y:row_idx 0 [] row
-
-let coord_to_str v : string =
-  match v with
-  | Point p -> Printf.sprintf "%d" p
-  | Range (x1,x2) -> Printf.sprintf "%d-%d" x1 x2
-
-let print_point (p : vertex) =
-  match p.v with
-  | Integer i -> Printf.printf "(%d | x: %s, y: %s)\n" i (coord_to_str p.x) (coord_to_str p.y)
-  | Symbol s -> Printf.printf "(%c | x: %s, y: %s)\n" s (coord_to_str p.x) (coord_to_str p.y)
+      scan (i + 1) line ~values:values ~symbols:symbols ~y:y
 
 
-(* let is_reachable v ~vertices = List.mem v vertices && v.x >= 0 && v.y >= 0 *)
+let process_row row_idx row : vertex list * symbol list = scan 0 row ~values:[] ~symbols:[] ~y:row_idx 
+
+let coord_to_str v : string = String.concat "," (List.map (fun s -> string_of_int s) v)
 
 
-let () = 
-  List.map print_point (List.flatten (List.mapi process_row map_rows)) |> (fun _ -> ())
-    (* List.filter (is_reachable ~vertices:vertices) vertices *)
-      (* |> List.map (fun v -> match v.v with | Integer i -> i | Symbol _ -> 0)
-      |> List.fold_left (+) 0
-      |> (fun v -> Printf.printf "Part 1 : %d\n" v)
-      |> (fun _ -> ()) *)
+(* let is_reachable v ~vertices = 
+  match v.v with
+  | Symbol _ -> false
+  | Integer _ -> 
+    let i_row_before = v.y *)
+
+
+let () =
+  let (values, symbols) = List.split (List.mapi process_row map_rows) in
+  let str_vals = List.map 
+    (fun p -> Printf.sprintf "(%d | x: %s, y: %d)" p.v (coord_to_str p.x) p.y) 
+    (List.flatten values) in
+  let str_syms = List.map 
+    (fun p -> Printf.sprintf "(x: %d, y: %d)" p.x p.y) 
+    (List.flatten symbols) in
+  print_endline (String.concat "\n" str_vals);
+  print_endline (String.concat "\n" str_syms)
+
